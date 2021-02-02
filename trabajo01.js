4cb71ce2ae99dccc6bbbdb3255ca5ce5f738bd9d
EGGC1998 <edgar132001@live.com.mx>
2021-02-02 06:10:54 -0600

    tipos de variables en trabajo01 y comense cicloFOR

---
+++ trabajo01.js
@@ -0,0 +1,15 @@
+var gera = 0;
+var gera1 = "gerardo";
+var gera2 = true;
+var gera3 = function gerardo(cosa,gera1) {
+        let gerardo = cosa + gera1;
+        return gerardo;
+
+    }
+
+
+
+var prueba = gera3("hola",gera1);
+
+prueba();
+
