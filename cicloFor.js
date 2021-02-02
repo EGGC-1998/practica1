530282513d54d8b4d28792b5fb551e669869d704
EGGC1998 <edgar132001@live.com.mx>
2021-02-02 06:35:51 -0600

    cICLOfOR, practicamos ciclo for 02/02/21

---
+++ cicloFor.js
@@ -0,0 +1,13 @@
+var invitados = ["el abulo" ,"mi compa el diego" ,"zurris" , "sergio", "batman" , "jon cena" , "superman" ,"mia kalifa" ,"pito perez" ,"la hermana de abuelo"];
+var cervezas = 60;
+var resultado = "";
+var contador = 0;
+
+for (let i = 0; i < invitados.length; i++) {
+
+    if (i == 4) {
+        resultado = invitados[i];
+    }
+
+    
+}
