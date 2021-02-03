cffea8da5da50821aed1e993c57a1515d2040ea2
EGGC1998 <edgar132001@live.com.mx>
2021-02-03 06:14:56 -0600

    Practica 2 del ciclo for , comparar variables , restar valores

--- cicloFor.js
+++ cicloFor.js
@@ -1,13 +1,15 @@
 var invitados = ["el abulo" ,"mi compa el diego" ,"zurris" , "sergio", "batman" , "jon cena" , "superman" ,"mia kalifa" ,"pito perez" ,"la hermana de abuelo"];
-var cervezas = 60;
+var cervezas = 24;
 var resultado = "";
 var contador = 0;
 
-for (let i = 0; i < invitados.length; i++) {
+for (let i = 0; cervezas >= 1; i++) {
 
-    if (i == 4) {
-        resultado = invitados[i];
+    if( i == 9 ){
+        i = 0;
     }
-
-    
+    cervezas--;
+    console.log(invitados[i]);
 }
+
+
